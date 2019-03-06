32ec68087dfe7e27be8c82a367aed969bc9e852f
Kayla <kchamilton2@crimson.ua.edu>
2019-03-05 21:33:30 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/connect_four.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/connect_four.dir/main.c.o"
+  "connect_four.pdb"
+  "connect_four.exe"
+  "libconnect_four.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/connect_four.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
